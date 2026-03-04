6d35364326dafec28d3401848488837a0e47b501
forybg <forrybg@yahoo.com>
2026-03-05 01:01:47 +0200

    fix installer to avoid private repo clone

--- install-edge.sh
+++ install-edge.sh
@@ -6,23 +6,23 @@ echo " Foritech Edge Installer"
 echo "---------------------------------"
 
 sudo apt update
-sudo apt install -y python3 python3-venv git
+sudo apt install -y python3 python3-venv python3-pip
 
-cd /opt
+INSTALL_DIR="/opt/foritech-edge"
 
-if [ ! -d "foritech-edge-runtime" ]; then
-    git clone https://github.com/forrybg/foritech-secure-system.git foritech-edge-runtime
-fi
-
-cd foritech-edge-runtime/foritech-edge
+sudo mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
 
 python3 -m venv venv
 source venv/bin/activate
 
-pip install .
+pip install --upgrade pip
+
+pip install git+https://github.com/forrybg/foritech-secure-system.git#subdirectory=foritech-edge
 
 echo ""
-echo "Foritech Edge installed successfully."
+echo "Foritech Edge installed."
 echo ""
 echo "Run:"
+echo "source /opt/foritech-edge/venv/bin/activate"
 echo "foritech-edge --help"
